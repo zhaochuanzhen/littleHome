9865a251f94341dbbf0a12a5ae848501079a9ac0
zhaochuanzhen <zhaochuanzhen@hxrainbow.com>
2020-05-20 18:17:20 +0800

    组件测试

---
+++ vue/src/components/about.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="abount">
+    <span>关于我们</span>
+    <!--    <router-view/>-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'abount',
+  data () {
+    return {
+      msg: 'Welcome to Your about.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  #abount span {
+    color: #42b983;
+  }
+</style>
--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import about from '@/components/about'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: about
     }
   ]
 })
